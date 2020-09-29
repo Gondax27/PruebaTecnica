411f52514b410aa53c0180e7997d1a0194186cf5
Juan José González Dávila <juanjogonda@gmail.com>
2020-09-28 20:28:56 -0500

    Prueba técnica

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className='contenedor'>
+      <button className='btn1'>DIAGNÓSTICO</button>
+      <button className='btn2'>PLANEACIÓN</button>
+      <button className='btn3'>EJECUCIÓN</button>
+      <button className='btn4'>SEGUIMIENTO Y EVALUACIÓN</button>
+      <button className='btn5'>RENDICIÓN DE CUENTAS</button>
+    </div>
+  );
+}
+
+export default App;
